598346b9e54317822fb17270da42773cc978d15d
mshahiq <m.shahiqmukhtar@gmail.com>
2021-09-30 15:00:16 +0500

    Visualizations-task

---
+++ Visualizations.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# Rating movies vs. Release Year
+
+#df2.plot.bar(stacked=True)
+plt.xlabel('Release Year')
+plt.ylabel('Rating')
+plt.title('Ratings of the Movies')
+plt.show()
+
+# no.of movies vs. director
+
+# pie chart 
+
+plt.xlabel('Director')
+plt.ylabel('Number of Movies')
+plt.title('Relation of directors and # of movies')
+plt.show()
+
+# no.of movies vs. actors
+
+# bar chart 
+# df2.plot.bar()
+
+plt.xlabel('Actor')
+plt.ylabel('Number of Movies')
+plt.title('Relation of actors and # of movies')
+
+plt.show()
