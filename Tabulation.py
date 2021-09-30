81600b7d75e3b13263ef32c143486028c4cb1b25
Harsha <harshavardhanch9@gmail.com>
2021-09-30 13:17:56 +0200

    Tabulation addition to main

---
+++ Tabulation.py
@@ -0,0 +1,19 @@
+# DataFrame of the IMDB 
+
+import pandas as pd
+import numpy as np
+
+# Normalized_max_min(Rating / Duration in mins)
+
+#(Z - np.mean (Z)) / (np.std (Z))
+
+#Zmin, Zmax = Z.min(), Z.max()
+
+
+# Normalized_mean(Rating / time to film )
+
+#(Z - np.mean (Z)) / (np.std (Z))
+
+# m = Z.mean()
+
+#
